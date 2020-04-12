76edfcfd85b243d4ee22929d9f1e023a8dba14ee
Artem <artemis.gorb.14@gmail.com>
2020-04-12 17:47:08 +0300

    Login, password & smth else

--- models.py
+++ models.py
@@ -34,6 +34,48 @@ class User(db.Model, Model):
         self.login = login
         self.password = password
 
+    def set_login(self, new_login):
+        self.login = new_login
+        self.save()
+
+    def set_password(self, new_pass):
+        self.password = new_pass
+        self.save()
+
+    @staticmethod
+    def check_login(login):
+        login = str(login)
+        if login in User.get_all_login():
+            return "User with this login already exists"
+        elif len(login) < 5:
+            return "Login must be at least 5 symbols"
+        elif not login[0].isalpha():
+            return "Login should starts with a letter"
+        else:
+            return 1
+
+    @staticmethod
+    def check_pass(password):
+        password = str(password)
+        if len(password) < 8:
+            return "Password should be at least 8 symbols"
+        elif password.isnumeric() or password.isalpha() or password.lower() == password or password.upper() == password:
+            return "Password should consists of at least: 1 upper case letter, 1 lower case and one number"
+        else:
+            return 1
+
+    @staticmethod
+    def get_login_by_id(i):
+        user = db.session.query(User).filter(User.id == i).first()
+        return user.login
+
+    @staticmethod
+    def get_all_login():
+        user_list = list(db.session.query(User))
+        login_list = []
+        for user in user_list:
+            login_list.append(user.id)
+
     @staticmethod
     def auth(login, password):
         return db.session.query(User).filter(User.login == login).filter(User.password == password).first()
@@ -53,7 +95,37 @@ class Task(db.Model, Model):
         self.author_id = author_id
         self.date = date
 
+    def set_status(self, status):
+        self.status = status
+        self.save()
 
+    def set_text(self, text):
+        self.text = text
+        self.save()
 
+    def set_name(self, name):
+        self.name = name
+        self.save()
+
+    @staticmethod
+    def get_task_by_id(i):
+        task = db.session.query(Task).filter(Task.id == i).first()
+        return {
+            "name": task.name,
+            "text": task.text,
+            "status": task.status,
+            "date": task.date,
+            "author_id": task.author_id,
+            "author_login": User.get_login_by_id(task.author_id)
+        }
+
+    @staticmethod
+    def get_users_tasks(user_id):
+        tasks = list(db.session.query(Task).filter(Task.author_id == user_id))
+        return_list = []
+        for task in tasks:
+            return_list.append(Task.get_task_by_id(task.id))
+
+        return return_list
 
 
